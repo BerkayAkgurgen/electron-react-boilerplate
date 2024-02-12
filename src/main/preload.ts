fd5059782e38706d64d48f092a27b9c20ad97454
Berkay Akgurgen <akgurgennberkay@gmail.com>
2024-02-12 12:27:34 +0300

    changes

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,5 +1,6 @@
 // Disable no-unused-vars, broken for spread args
 /* eslint no-unused-vars: off */
+import { channel } from 'diagnostics_channel';
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
 export type Channels = 'ipc-example';
@@ -24,6 +25,19 @@ const electronHandler = {
   },
 };
 
+const apiHandler = {
+  ipcRenderer: {
+    send: (channel: Channels, data: unknown) => {
+      ipcRenderer.send(channel, data);
+    },
+    receive: (channel: Channels, func: (...args: unknown[]) => void) => {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    },
+  }
+};
+
 contextBridge.exposeInMainWorld('electron', electronHandler);
+contextBridge.exposeInMainWorld('api', apiHandler);
 
 export type ElectronHandler = typeof electronHandler;
+export type ApiHandler = typeof apiHandler;
